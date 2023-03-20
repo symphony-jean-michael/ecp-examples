7afc2483de9c9a8980c695ae176747ae18e3b25a
Jean-Michael Legait <jean-michael.legait@symphony.com>
2023-03-20 17:01:04 +0100

    Use corporate streamIds

--- AppExamples/CleverDeal.React/src/Data/deals.ts
+++ AppExamples/CleverDeal.React/src/Data/deals.ts
@@ -40,7 +40,7 @@ const generateDeals = (n: number): DashboardItemInterface[] => {
         riskLevel: 'Medium',
         type: 'Whatever',
         minimum: '$100k',
-        roomId: {}
+        roomId: ''//{}
       }
     })
   }
@@ -50,10 +50,11 @@ const generateDeals = (n: number): DashboardItemInterface[] => {
 export const deals: DashboardItemInterface[] = [
   {dealId: '123456', lastUpdated: 'Today', status: 'active', name: 'Biot investment', details: {
     members: [yannick, thomas, zimmy],
-    roomId: {
+    roomId: 'ukiBBTWrfSJf8aJG8bri7H///nkAVF7ldA==',
+    /*{
       'st3.symphony.com': 'SiuRpjsdymb4e53zKzuFkn///nkVPUAmdA==',//Cm2ZKi4E3qdH8NqUt4BHQH///oCvvlv6dA==',
       'corporate.symphony.com': '2n/aK4gQ3S8VnXPgPbXzF3///oCcFZezdA=='
-    },
+    },*/
     country: 'France',
     riskLevel: 'Medium',
     type: 'Equity',
@@ -62,10 +63,11 @@ export const deals: DashboardItemInterface[] = [
   ...generateDeals(2),
   {dealId: '654321', lastUpdated: 'Yesterday', status: 'active', name: 'Sophia investment', details: {
     members: [yannick, antoine, thomas],
-    roomId: {
+    roomId: '933USqu3iSZZMmDUk0w1xH///nkAU90YdA==',
+    /*{
       'st3.symphony.com': 'Le/SskG3q6p4oW9bj6WTq3///nkVMxdpdA==',//'egJ5XAdOKzC+1f42Zhewy3///oCvvdYAdA==',
       'corporate.symphony.com': 'eboDMTcQT4GsDk6AjQP3zn///n8cy6jVdA=='
-    },
+    },*/
     country: 'China',
     riskLevel: 'High',
     type: 'Commodity Future',
@@ -74,10 +76,11 @@ export const deals: DashboardItemInterface[] = [
   ...generateDeals(3),
   {dealId: '789103', lastUpdated: '2 days ago', status: 'active', name: 'Valbonne investment', details: {
     members: [yannick, antoine],
-    roomId: {
+    roomId: 'egJ5XAdOKzC+1f42Zhewy3///oCvvdYAdA==',
+    /*{
       'st3.symphony.com': 'egJ5XAdOKzC+1f42Zhewy3///oCvvdYAdA==',
       'corporate.symphony.com': 'm4okqNL/fAtDdRBhVOxloX///n8cyzwNdA=='
-    },
+    },*/
     country: 'USA',
     riskLevel: 'Low',
     type: 'Corporate Bond',
