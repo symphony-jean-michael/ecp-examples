f6f77f574c21fa08e5901f72737559c67ef4aef8
Jean-Michael Legait <jean-michael.legait@symphony.com>
2023-03-23 13:32:24 +0100

    Add missing streamId

--- AppExamples/CleverDeal.React/src/Data/deals.ts
+++ AppExamples/CleverDeal.React/src/Data/deals.ts
@@ -90,7 +90,7 @@ export const deals: DashboardItemInterface[] = [
     roomId: 'w3wFsyhAB71H4UidbvraI3///nkAOr4kdA==',
 
     // DEMO: 'LsaOJ4qUpyKanAhVYtcTd3///nj2NAQddA=='
-    // JM TEST: 
+    // JM TEST: 'w3wFsyhAB71H4UidbvraI3///nkAOr4kdA=='
 
     /*{
       'st3.symphony.com': 'egJ5XAdOKzC+1f42Zhewy3///oCvvdYAdA==',
